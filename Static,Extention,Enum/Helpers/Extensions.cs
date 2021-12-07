d210c0d0d2f5106a49a827deeaf0556328fc1fc0
Rashad Gasimzada <rashad.gasimzade.2000@gmail.com>
2021-12-07 23:04:55 +0400

    Extension added

---
+++ Static,Extention,Enum/Helpers/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Static_Extention_Enum.Helpers
+{
+    public static class Extensions
+    {
+        public static int StringCheck(this string str,string pattern)
+        {
+            return Regex.Matches(str, pattern).Count;
+        }
+    }
+}
---
+++ Static,Extention,Enum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Static_Extention_Enum.Helpers;
+namespace Static_Extention_Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Salam Rashad,necesen Rashad";
+            int result = str.StringCheck("Rashad");
+            Console.WriteLine(result);
+        }
+    }
+}
